f431d7b2b08b236208788d1c5a6abdd12cf8ba50
erw1nhmtz <fhntvvfyeyby@outlook.com>
2024-11-21 17:42:46 +0200

    Added code for completed lectures

--- 02-Fundamentals-Part-2/assignments.js
+++ 02-Fundamentals-Part-2/assignments.js
@@ -42,24 +42,99 @@
 // console.log(juiceMaker(`blueberry`, 100));
 // console.log(juiceMaker(`tires`, 3));
 
+
 // * 2. Function Declarations vs. Function Expressions
 
-function percentageOfWorld1(population) {
+// function percentageOfWorld1(population) {
+//     const worldPopulation = 7900;
+//     return `${(population / worldPopulation) * 100}%`;
+// }
+
+// const chinaPercentage = percentageOfWorld1(1441);
+// const ukrainePercentage = percentageOfWorld1(45);
+// const francePercentage = percentageOfWorld1(65);
+
+// console.log(`China - ${chinaPercentage}, Ukraine - ${ukrainePercentage}, France - ${francePercentage}`);
+
+// const percentageOfWorld2 = function (population) {
+//     const worldPopulation = 7900;
+//     return `${(population / worldPopulation) * 100}%`;
+// };
+
+// console.log(percentageOfWorld2(1441));
+// console.log(percentageOfWorld2(45));
+// console.log(percentageOfWorld2(65));
+
+
+// * 3. Arrow Functions
+
+// const percentageOfWorld3 = population => (population / 7900) * 100;
+
+// console.log(percentageOfWorld3(1441));
+
+// const canMakeCar = (carModel, carParts) => {
+//     const ladaPartsNeeded = 25;
+//     const bmwPartsNeeded = 50;
+//     const mercedezPartsNeeded = 100;
+//     const teslaPartsNeeded = 150;
+
+//     switch (carModel) {
+//         case `lada`:
+//             return carParts >= ladaPartsNeeded ? true : false;
+//         case `bmw`:
+//             return carParts >= bmwPartsNeeded ? true : false;
+//         case `mercedez`:
+//             return carParts >= mercedezPartsNeeded ? true : false;
+//         case `tesla`:
+//             return carParts >= teslaPartsNeeded ? true : false;
+//         default:
+//             return false;
+//     }
+// }
+
+// console.log(`Can I make a lada ? - ${canMakeCar(`lada`, 25)}`);
+// console.log(`Can I make a mercedez ? - ${canMakeCar(`mercedez`, 90)}`);
+// console.log(`Can I make a fiat? - ${canMakeCar(`fiat`, 10000)}`);
+
+// const canDrive = (firstName, hasDriversLicense, hasGoodVision, isTired) => {
+//     if (hasDriversLicense && hasGoodVision && !isTired) return `${firstName} has a drivers license, a good vision and is not tired, therefore, can drive a car!`;
+
+//     if (!hasDriversLicense) return `${firstName} doesn't have a drivers license !`;
+//     else if (!hasGoodVision) return `${firstName} has a bad vision !`;
+//     else return `${firstName} is too tired to drive a car!`;
+// }
+
+// console.log(canDrive(`Sarah`, true, true, false));
+// console.log(canDrive(`Jonas`, true, true, true));
+// console.log(canDrive(`Chad`, true, false, false));
+// console.log(canDrive(`Sarah`, false, true, false));
+
+
+// * 4. Functions Calling Other Functions
+
+const percentageOfWorld1 = function (population) {
     const worldPopulation = 7900;
-    return `${(population / worldPopulation) * 100}%`;
+    return (population / worldPopulation) * 100;
 }
 
-const chinaPercentage = percentageOfWorld1(1441);
-const ukrainePercentage = percentageOfWorld1(45);
-const francePercentage = percentageOfWorld1(65);
+// Function Declaration
+function describePopulation1(country, population) {
+    const percentage = percentageOfWorld1(population);
+    return `${country} has ${population} million people, which is about ${percentage}% of the world.`;
+}
 
-console.log(`China - ${chinaPercentage}, Ukraine - ${ukrainePercentage}, France - ${francePercentage}`);
-
-const percentageOfWorld2 = function (population) {
-    const worldPopulation = 7900;
-    return `${(population / worldPopulation) * 100}%`;
+// Function Expression
+const describePopulation2 = function (country, population) {
+    const percentage = percentageOfWorld1(population);
+    return `${country} has ${population} million people, which is about ${percentage}% of the world.`;
 };
 
-console.log(percentageOfWorld2(1441));
-console.log(percentageOfWorld2(45));
-console.log(percentageOfWorld2(65));
+// Arrow Function
+const describePopulation3 = (country, population) => {
+    const percentage = percentageOfWorld1(population);
+    return `${country} has ${population} million people, which is about ${percentage}% of the world.`
+}
+
+console.log(describePopulation1(`Ukraine`, 45));
+console.log(describePopulation2(`China`, 1441));
+console.log(describePopulation3(`USA`, 338));
--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -41,43 +41,132 @@
 
 // * 3. Function Declarations vs. Expressions
 
-// Function Declaration
-function calcAge1(birthYear) {
-    return 2024 - birthYear;
+// // Function Declaration
+// function calcAge1(birthYear) {
+//     return 2024 - birthYear;
+// }
+
+// console.log(calcAge1(2015));
+// console.log(calcAge1(2005));
+// console.log(calcAge1(1991));
+
+// // Function Expression
+// const calcAge2 = function (birthYear) {
+//     return 2037 - birthYear;
+// };
+// const age2 = calcAge2(1991);
+// console.log(age2);
+
+// const canMakeCar = function (carModel, carParts) {
+//     const ladaPartsNeeded = 15;
+//     const bmwPartsNeeded = 25;
+//     const mercedezPartsNeeded = 50;
+//     const teslaPartsNeeded = 100;
+
+//     switch (carModel) {
+//         case `lada`:
+//             return carParts >= ladaPartsNeeded ? true : false;
+//         case `bmw`:
+//             return carParts >= bmwPartsNeeded ? true : false;
+//         case `mercedez`:
+//             return carParts >= mercedezPartsNeeded ? true : false;
+//         case `tesla`:
+//             return carParts >= teslaPartsNeeded ? true : false;
+//         default:
+//             return false;
+//     }
+// };
+
+// console.log(`Can make a lada ? - ${canMakeCar(`lada`, 15)}`);
+// console.log(`Can make a bmw ? - ${canMakeCar(`bmw`, 20)}`);
+// console.log(`Can make a mercedez ? - ${canMakeCar(`mercedez`, 55)}`);
+// console.log(`Can make a tesla ? - ${canMakeCar(`tesla`, 125)}`);/
+
+
+// * 4. Arrow functions
+
+// // Function expression
+// const calcAge2 = function (birthYear) {
+//     return 2037 - birthYear;
+// }
+
+// // Arrow function
+// const calcAge3 = (birthYear) => 2037 - birthYear;
+// console.log(calcAge3(1991));
+
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//     const age = 2037 - birthYear;
+//     const retirement = 65 - age;
+
+//     return `${firstName} retires in ${retirement} years`;
+// };
+
+// console.log(yearsUntilRetirement(1991, `Jonas`));
+// console.log(yearsUntilRetirement(1980, `Chad`));
+
+
+// * 5. Functions Calling Other Functions
+
+// function cutFruitPieces(fruit) {
+//     return fruit * 4;
+// }
+
+// function fruitProcessor(apples, oranges) {
+//     const applePieces = cutFruitPieces(apples);
+//     const orangePieces = cutFruitPieces(oranges);
+
+//     const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+//     return juice;
+// }
+
+// console.log(fruitProcessor(2, 3));
+
+
+// *  6. Reviewing Functions
+
+// const calcAge = function (birthYear) {
+//     const currentYear = 2037;
+//     return currentYear - birthYear;
+// }
+
+// const yearsUntilRetirement = function (birthYear, firstName) {
+//     const age = calcAge(birthYear);
+//     const retirement = 65 - age;
+
+//     if (retirement > 0) return retirement;
+//     else return -1;
+// }
+
+// console.log(calcAge(1991));
+// console.log(yearsUntilRetirement(1991, `Jonas`));
+// console.log(yearsUntilRetirement(1950, `Mike`));
+
+
+// * Challenge #1
+
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+
+// Test 1
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
+
+console.log(scoreDolphins, scoreKoalas);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+    if (avgDolphins >= avgKoalas * 2) {
+        console.log(`Dolphins win the trophy🏆 (${avgDolphins} vs. ${avgKoalas})`);
+    } else if (avgKoalas >= avgDolphins * 2) {
+        console.log(`Koalas win the trophy 🏆 (${avgKoalas} vs. ${avgDolphins})`);
+    } else {
+        console.log(`No team wins...`);
+    }
 }
 
-console.log(calcAge1(2015));
-console.log(calcAge1(2005));
-console.log(calcAge1(1991));
+checkWinner(scoreDolphins, scoreKoalas);
 
-// Function Expression
-const calcAge2 = function (birthYear) {
-    return 2037 - birthYear;
-};
-const age2 = calcAge2(1991);
-console.log(age2);
+// Test 2
+scoreDolphins = calcAverage(85, 54, 41);
+scoreKoalas = calcAverage(23, 34, 27);
+console.log(scoreDolphins, scoreKoalas);
 
-const canMakeCar = function (carModel, carParts) {
-    const ladaPartsNeeded = 15;
-    const bmwPartsNeeded = 25;
-    const mercedezPartsNeeded = 50;
-    const teslaPartsNeeded = 100;
-
-    switch (carModel) {
-        case `lada`:
-            return carParts >= ladaPartsNeeded ? true : false;
-        case `bmw`:
-            return carParts >= bmwPartsNeeded ? true : false;
-        case `mercedez`:
-            return carParts >= mercedezPartsNeeded ? true : false;
-        case `tesla`:
-            return carParts >= teslaPartsNeeded ? true : false;
-        default:
-            return false;
-    }
-};
-
-console.log(`Can make a lada ? - ${canMakeCar(`lada`, 15)}`);
-console.log(`Can make a bmw ? - ${canMakeCar(`bmw`, 20)}`);
-console.log(`Can make a mercedez ? - ${canMakeCar(`mercedez`, 55)}`);
-console.log(`Can make a tesla ? - ${canMakeCar(`tesla`, 125)}`);
+checkWinner(scoreDolphins, scoreKoalas);
