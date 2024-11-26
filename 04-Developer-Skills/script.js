94bf3d66c298e82f29b53a2addc6ac285ae33cce
erw1nhmtz <fhntvvfyeyby@outlook.com>
2024-11-26 16:39:15 +0200

    Started new section of the course. Added starter code for it.

---
+++ 04-Developer-Skills/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const x = 23;
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+};
+
+console.log(`Hello World`);
+console.log(calcAge(1991));
+console.log(calcAge(2005));
