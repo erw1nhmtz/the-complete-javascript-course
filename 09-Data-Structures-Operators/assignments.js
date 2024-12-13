2bdcad394b9bb422f9c0579188907ee03fec382a
erw1nhmtz <fhntvvfyeyby@outlook.com>
2024-12-13 19:07:33 +0200

    Completed some more lectures.

--- 09-Data-Structures-Operators/assignments.js
+++ 09-Data-Structures-Operators/assignments.js
@@ -1,13 +1,38 @@
 'use strict';
 
-// * Destructuring Arrays
+// * 1. Destructuring Arrays
 
-const books = [`Games of Thrones`, `Clash of Kings`, `Storm of Swords`, `Feast of Crows`, `Dance of Dragons`];
+// const books = [`Games of Thrones`, `Clash of Kings`, `Storm of Swords`, `Feast of Crows`, `Dance of Dragons`];
 
-// ! DO THIS FIRST, AND THEN WITH THE OBJECTS
+// // * 1.1 
+
+// const [firstBook, secondBook] = books;
+// console.log(firstBook, secondBook);
 
 
-// * Destructuring Objects
+// // * 1.2
+
+// const [, , thirdBook] = books;
+// console.log(thirdBook); 
+
+
+// // * 1.3
+
+// const ratings = [[`rating`, 4.19], [`ratingsCount`, 144584]];
+
+// const [[, rating], [, ratingsCount]] = ratings;
+// console.log(rating, ratingsCount); 
+
+
+// // * 1.4
+
+// const ratingStars = [63405, 1808];
+
+// const [fiveStarRatings, oneStarRatings, threeStarRatings = 0] = ratingStars;
+// console.log(fiveStarRatings, oneStarRatings, threeStarRatings);
+
+
+// * 2. Destructuring Objects
 
 // const books = [
 //     {
@@ -48,6 +73,291 @@ const books = [`Games of Thrones`, `Clash of Kings`, `Storm of Swords`, `Feast o
 //         author: `George R.R. Martin`,
 //         ISBN: 9373546023,
 //         keywords: [`beep`, `bop`, `boop`],
-//         language: `English`,
+//         language: `English`
 //     }
 // ];
+
+// // * 2.1
+
+// const {title, author, ISBN} = books[0];
+// console.log(title, author, ISBN);
+
+
+// // * 2.2
+
+// const {keywords: tags} = books[0];
+// console.log(tags);
+
+
+// // * 2.3
+
+// const {language, programmingLanguage = `unknown`} = books[4];
+// console.log(language, programmingLanguage);
+
+
+// // * 2.4
+// console.log(`*****************`);
+
+// let bookTitle = `unknown`;
+// let bookAuthor = `unknown`;
+
+// ({title: bookTitle, author: bookAuthor} = books[0]);
+// console.log(bookTitle, bookAuthor);
+
+
+// // * 2.5
+
+// const {thirdParty: {goodreads: {rating}}} = books[0];
+// console.log(rating);
+
+
+// // * 2.6
+
+// const printBookInfo = function({title, author, year = `year unknown`})  { // Second way
+//     // First way
+//     if (!year) year = `year unknown`;
+//     console.log(`${title} by ${author}, ${year}`);
+// };
+
+// printBookInfo({title: `Algorithms`, author: `Robert Sedgewick`, year: `2011`});
+// printBookInfo({title: `Game of Thrones`, author: `George R.R. Martin`});
+
+
+// * 3 The Spread Operator
+
+// * 3.1
+
+// const books = [
+//     {
+//         title: `Game of Thrones`,
+//         author: `George R.R. Martin`,
+//         ISBN: 6322983997,
+//         keywords: [`beep`, `beep`, `boop`],
+//         thirdParty: {
+//             goodreads: {
+//               rating: 4.41,              // <-- HERE
+//               ratingsCount: 1733,
+//               reviewsCount: 63,
+//               fiveStarRatingCount: 976,
+//               oneStarRatingCount: 13
+//             }
+//         }
+//     },
+//     {
+//         title: `Clash of Kings`,
+//         author: [`George R.R. Martin`, `Some other dude`],
+//         ISBN: 6504187002,
+//         keywords: [`beep`, `bop`, `beep`]
+//     },
+//     {
+//         title: `Storm of Swords`,
+//         author: [`George R.R. Martin`, `Some other dude`, `Some other dude 2`],
+//         ISBN: 1439013314,
+//         keywords: [`beep`, `bop`, `beep`, `bop`]
+//     },
+//     {
+//         title: `Feast of Crows`,
+//         author: `George R.R. Martin`,
+//         ISBN: 5586956527,
+//         keywords: [`bop`, `bop`, `beep`]
+//     },
+//     {
+//         title: `Dance with the Dragons`,
+//         author: `George R.R. Martin`,
+//         ISBN: 9373546023,
+//         keywords: [`beep`, `bop`, `boop`],
+//         language: `English`
+//     }
+// ];
+
+// const bookAuthors = [books[0].author, ...books[2].author];
+// console.log(bookAuthors);
+
+
+// // * 3.2
+
+// const spellWord = function(str) {
+//     console.log(...str);
+// }
+
+// spellWord(`JavaScript`);
+
+
+// * 4. Rest Pattern and Parameters
+
+// const books = [
+//     {
+//         title: `Game of Thrones`,
+//         author: `George R.R. Martin`,
+//         ISBN: 6322983997,
+//         keywords: [`beep`, `beep`, `boop`],
+//         publisher: `Bantam books`,
+//         thirdParty: {
+//             goodreads: {
+//               rating: 4.41,              // <-- HERE
+//               ratingsCount: 1733,
+//               reviewsCount: 63,
+//               fiveStarRatingCount: 976,
+//               oneStarRatingCount: 13
+//             }
+//         }
+//     },
+//     {
+//         title: `Clash of Kings`,
+//         author: [`George R.R. Martin`, `Some other dude`],
+//         ISBN: 6504187002,
+//         publisher: `Bantam books`,
+//         keywords: [`beep`, `bop`, `beep`]
+//     },
+//     {
+//         title: `Storm of Swords`,
+//         author: [`George R.R. Martin`, `Some other dude`, `Some other dude 2`],
+//         ISBN: 1439013314,
+//         keywords: [`beep`, `bop`, `beep`, `bop`]
+//     },
+//     {
+//         title: `Feast of Crows`,
+//         author: `George R.R. Martin`,
+//         ISBN: 5586956527,
+//         keywords: [`bop`, `bop`, `beep`]
+//     },
+//     {
+//         title: `Dance with the Dragons`,
+//         author: `George R.R. Martin`,
+//         ISBN: 9373546023,
+//         keywords: [`beep`, `bop`, `boop`],
+//         language: `English`
+//     }
+// ];
+
+// // * 4.1
+
+// const [mainKeyword, ...rest] = books[0].keywords;
+// console.log(mainKeyword, rest);
+// console.log(mainKeyword, ...rest);
+
+
+// // * 4.2
+
+// const {publisher: bookPublisher, ...restOfTheBook} = books[1];
+// console.log(bookPublisher, restOfTheBook);
+// // ! Unable to use "spread operator" on object restOfTheBook
+// // console.log(bookPublisher, ...restOfTheBook);
+
+
+// // * 4.3
+
+// const printBookAuthorsCount = function(title, ...authors) {
+//     console.log(`The book ${title} has ${authors.length} authors`);
+// };
+
+// printBookAuthorsCount(`Algorithms`, `Robert Sedgewick`, `Kevin Wayne`);
+
+
+// * 5. Short Cicruiting (&& and ||)
+
+const books = [
+    {
+        title: `Game of Thrones`,
+        author: `George R.R. Martin`,
+        ISBN: 6322983997,
+        keywords: [`beep`, `beep`, `boop`],
+        publisher: `Bantam books`,
+        onlineContent: true,
+        highlighted: true,
+        edition: 3,
+        thirdParty: {
+            goodreads: {
+              rating: 4.41,              // <-- HERE
+              ratingsCount: 1733,
+              reviewsCount: 63,
+              fiveStarRatingCount: 976,
+              oneStarRatingCount: 13
+            }
+        }
+    },
+    {
+        title: `Clash of Kings`,
+        author: [`George R.R. Martin`, `Some other dude`],
+        ISBN: 6504187002,
+        onlineContent: false,
+        edition: undefined,
+        publisher: `Bantam books`,
+        keywords: [`beep`, `bop`, `beep`],
+        highlighted: true,
+        programmingLanguage: `Java`
+    },
+    {
+        title: `Storm of Swords`,
+        author: [`George R.R. Martin`, `Some other dude`, `Some other dude 2`],
+        onlineContent: false,
+        edition: undefined,
+        ISBN: 1439013314,
+        highlighted: true,
+        keywords: [`beep`, `bop`, `beep`, `bop`],
+        programmingLanguage: `Kotlin`
+    },
+    {
+        title: `Feast of Crows`,
+        author: `George R.R. Martin`,
+        edition: undefined,
+        ISBN: 5586956527,
+        highlighted: true, 
+        keywords: [`bop`, `bop`, `beep`]
+    },
+    {
+        title: `Dance with the Dragons`,
+        author: `George R.R. Martin`,
+        edition: undefined,
+        ISBN: 9373546023,
+        keywords: [`beep`, `bop`, `boop`],
+        highlighted: true,
+        language: `English`
+    }
+];
+
+// // * 5.1
+
+// const hasExamplesInJava = function(book) {
+//     return book.programmingLanguage === `Java` || `no data available`;  
+// };
+
+// console.log(hasExamplesInJava(books[0]));
+// console.log(hasExamplesInJava(books[1]));
+// console.log(hasExamplesInJava(books[2]));
+
+
+// // * 5.2
+
+// for(let i = 0; i < books.length; i++) {
+//     books[i].onlineContent && console.log(`${books[i].title} provides online content.`);
+// }
+
+
+// * 6. The Nullish Coalescing Operator (??)
+
+// // * 6.1
+
+// for (let i = 0; i < books.length; i++) {
+//     books[i].onlineContent ?? console.log(`${books[i].title} provides no data about its online content`);
+// }
+
+
+// * 7. Logical Assignments Operators
+
+// * 7.1
+
+for (let i = 0; i < books.length; i++) {
+    books[i].edition ||= 1;
+}
+
+
+// * 7.2
+
+for (let i = 0; i < books.length; i++) {
+    books[i].highlighted &&= !(books[i].thirdParty.goodreads.rating < 4.2);
+}
+
+for (let i = 0; i < books.length; i++) {
+    console.log(books[i]);
+}
--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -55,6 +55,249 @@
 
 // * 2. Destructuring Objects
 
+// const restaurant = {
+//     name: `Classico Italiano`,
+//     location: `Via Angelo Tavanti 23, Firenze, Italy`,
+//     categories: [`Italian`, `Pizzeria`, `Vegetarian`, `Organic`],
+//     starterMenu: [`Focaccia`, `Bruschetta`, `Garlic Bread`, `Caprese Salad`],
+//     mainMenu: [`Pizza`, `Pasta`, `Risotto`],
+//     openingHours: {
+//         thu: {
+//             open: 12,
+//             close: 22
+//         },
+//         fri: {
+//             open: 11,
+//             close: 23
+//         },
+//         sat: {
+//             open: 0, // Open 24 hours
+//             close: 24
+//         }
+//     },
+
+//     order: function(starterIndex, mainIndex) {
+//         return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+//     },
+
+//     orderDelivery: function({starterIndex = 1, mainIndex = 0, time = `20:00`, address}) {
+//         console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+//     }
+// };
+
+// restaurant.orderDelivery({
+//     time: `22:30`,
+//     address: `Via del Sole, 21`,
+//     mainIndex: 2,
+//     starterIndex: 2
+// });
+
+// restaurant.orderDelivery({
+//     address: `Via del Sole, 21`,
+//     starterIndex: 1
+// });
+
+// const {name, openingHours, categories} = restaurant;
+// console.log(name, openingHours, categories);
+
+// const {name: restaurantName, openingHours: hours, categories: tags} = restaurant;
+// console.log(restaurantName, hours, tags);
+
+// // Default values
+// const { menu = [], starterMenu: starters = []} = restaurant;
+// console.log(menu, starters);
+
+// // Mutating variables
+// let a = 111;
+// let b = 999;
+// const obj = {
+//     a: 23, 
+//     b: 7, 
+//     c: 14
+// };
+
+// ({a, b} = obj);
+// console.log(a, b);
+
+// // Nested objects
+// const { fri: {open: o, close: c} } = openingHours;
+// console.log(o, c);
+
+
+// * 3. The Spread Operator (...)
+
+// const restaurant = {
+//     name: `Classico Italiano`,
+//     location: `Via Angelo Tavanti 23, Firenze, Italy`,
+//     categories: [`Italian`, `Pizzeria`, `Vegetarian`, `Organic`],
+//     starterMenu: [`Focaccia`, `Bruschetta`, `Garlic Bread`, `Caprese Salad`],
+//     mainMenu: [`Pizza`, `Pasta`, `Risotto`],
+//     openingHours: {
+//         thu: {
+//             open: 12,
+//             close: 22
+//         },
+//         fri: {
+//             open: 11,
+//             close: 23
+//         },
+//         sat: {
+//             open: 0, // Open 24 hours
+//             close: 24
+//         }
+//     },
+
+//     order: function(starterIndex, mainIndex) {
+//         return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+//     },
+
+//     orderDelivery: function({starterIndex = 1, mainIndex = 0, time = `20:00`, address}) {
+//         console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+//     },
+
+//     orderPasta: function(ing1, ing2, ing3) {
+//         console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
+//     }
+// };
+
+// const arr = [7, 8, 9];
+// // Bad way
+// const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+// // Good way
+// const newArr = [1, 2, ...arr];
+
+// console.log(badNewArr);
+// console.log(newArr);
+
+// console.log(...newArr);
+
+// const newMenu = [...restaurant.mainMenu, `Gnocci`];
+// console.log(newMenu);
+// console.log(...newMenu);
+
+// // Copy Array
+// const mainMenuCopy = [...restaurant.mainMenu];
+// console.log(mainMenuCopy);
+
+// // Join 2 Arrays
+// const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+// console.log(menu);
+
+// // Iterables: arrays, strings, sets, maps. NOT ALL objects
+// const str = `Jonas`;
+// const letters = [...str, ``, `S.`];
+// console.log(letters);
+
+// const ingredients = [
+//     window.prompt(`Lets's make pasta. Ingredient 1`), 
+//     window.prompt(`Ingredient 2`),
+//     window.prompt(`Ingredient 3`)  
+// ];
+
+// console.log(ingredients);
+// // Old way, lame
+// restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
+// // New way, rizz
+// restaurant.orderPasta(...ingredients);
+
+
+// Objects
+
+// const newRestaurant = {...restaurant, founder: `Guiseppe`, testProperty: undefined};
+
+// console.table(newRestaurant);
+
+// const restaurantCopy = {...restaurant};
+// restaurantCopy.name = `Restaurante Roma`;
+// console.log(restaurantCopy.name, restaurant.name);
+
+
+// * 4. Rest Pattern and Parameters
+
+// const restaurant = {
+//     name: `Classico Italiano`,
+//     location: `Via Angelo Tavanti 23, Firenze, Italy`,
+//     categories: [`Italian`, `Pizzeria`, `Vegetarian`, `Organic`],
+//     starterMenu: [`Focaccia`, `Bruschetta`, `Garlic Bread`, `Caprese Salad`],
+//     mainMenu: [`Pizza`, `Pasta`, `Risotto`],
+//     openingHours: {
+//         thu: {
+//             open: 12,
+//             close: 22
+//         },
+//         fri: {
+//             open: 11,
+//             close: 23
+//         },
+//         sat: {
+//             open: 0, // Open 24 hours
+//             close: 24
+//         }
+//     },
+
+//     order: function(starterIndex, mainIndex) {
+//         return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+//     },
+
+//     orderDelivery: function({starterIndex = 1, mainIndex = 0, time = `20:00`, address}) {
+//         console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+//     },
+
+//     orderPasta: function(ing1, ing2, ing3) {
+//         console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
+//     },
+
+//     orderPizza: function(mainIngredient, ...otherIngredients) {
+//         console.log(mainIngredient);
+//         console.log(otherIngredients);
+//     }
+// };
+
+// // Spread because on RIGHT side of =
+// const arr = [1, 2, ...[3, 4]];
+// console.log(arr);
+
+// const [a, b, ...others] = [1, 2, 3, 4, 5]; 
+// console.log(a, b, others);
+// console.log(a, b, ...others);
+
+// const [pizza, , risotto, ...otherFood] = [...restaurant.mainMenu, ...restaurant.starterMenu];
+// console.log(pizza, risotto, otherFood);
+
+// // Objects
+// const {sat, ...weekdays} = restaurant.openingHours;
+// console.log(weekdays);
+
+// // 2) Functions
+// const add = function(...numbers) {
+//     let sum = 0;
+//     for (let i = 0; i < numbers.length; i++) {
+//         sum += numbers[i];
+//     }
+//     return sum;
+// };
+ 
+// console.log(add(2, 3));
+// console.log(add(5, 3, 7, 5));
+
+// const x = [23, 5, 7];
+// console.log(add(...x));
+
+// restaurant.orderPizza(`mushrooms`, `onion`, `olives`, `spinach`);
+// restaurant.orderPizza(`Mushrooms`);
+
+
+// * 5. Short Circuiting (&& and ||)
+
+// Use ANY data type, return ANY data type
+// short-circuiting
+// console.log(3 || `Jonas`);
+// console.log(`` || `Jonas`);
+// console.log(true || 0);
+// console.log(undefined || null);
+
+// console.log(undefined || null || `` || `Hello` || 23 || 0);
+
 const restaurant = {
     name: `Classico Italiano`,
     location: `Via Angelo Tavanti 23, Firenze, Italy`,
@@ -82,43 +325,73 @@ const restaurant = {
 
     orderDelivery: function({starterIndex = 1, mainIndex = 0, time = `20:00`, address}) {
         console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+    },
+
+    orderPasta: function(ing1, ing2, ing3) {
+        console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
+    },
+
+    orderPizza: function(mainIngredient, ...otherIngredients) {
+        console.log(mainIngredient);
+        console.log(otherIngredients);
     }
 };
 
-restaurant.orderDelivery({
-    time: `22:30`,
-    address: `Via del Sole, 21`,
-    mainIndex: 2,
-    starterIndex: 2
-});
+// restaurant.numGuests = 23;
+// const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
 
-restaurant.orderDelivery({
-    address: `Via del Sole, 21`,
-    starterIndex: 1
-});
+// console.log(guests1);
 
-const {name, openingHours, categories} = restaurant;
-console.log(name, openingHours, categories);
+// const guest2 = restaurant.numGuests || 10;
+// console.log(guest2);
 
-const {name: restaurantName, openingHours: hours, categories: tags} = restaurant;
-console.log(restaurantName, hours, tags);
+// console.log(`----------AND-----------`);
+// console.log(0 && `Jonas`);
 
-// Default values
-const { menu = [], starterMenu: starters = []} = restaurant;
-console.log(menu, starters);
+// console.log(`Hello` && 23 && null && `jonas`);
 
-// Mutating variables
-let a = 111;
-let b = 999;
-const obj = {
-    a: 23, 
-    b: 7, 
-    c: 14
+// // Practical example
+// if (restaurant.orderPizza) {
+//     restaurant.orderPizza(`mushrooms`, `spinach`);
+// }
+
+// restaurant.orderPizza && restaurant.orderPizza(`mushrooms`, `spinach`);
+
+
+// * 6. The nullish Coalescing Operator (??)
+
+// restaurant.numGuests = 0;
+// const guests = restaurant.numGuests || 10;
+// console.log(guests);
+
+// // Nullish: null and undefined (NOT 0 or ``)
+// const guestCorrect = restaurant.numGuests ?? 10;
+// console.log(guestCorrect);
+
+
+// * 7. Logical Assignment Operators
+
+const rest1 = {
+    name: `Capri`,
+    numGuests: 20,
 };
 
-({a, b} = obj);
-console.log(a, b);
+const rest2 = {
+    name: `La Piazza`,
+    owner: `Giovanni Rossi`,
+    numGuests: undefined
+};
 
-// Nested objects
-const { fri: {open: o, close: c} } = openingHours;
-console.log(o, c);
+rest2.numGuests ??= 10;
+
+console.log(rest1);
+console.log(rest2);
+
+// rest1.owner = rest1.owner && `<ANONYMOUS>`;
+
+// Old way
+// rest2.owner = rest2.owner && `<ANONYMOUS>`;
+
+// New way 
+rest2.owner &&= `ANONYMOUS`;
+console.log(rest2);
